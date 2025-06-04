f37cc6dfae5d669c3e39328e7dc470cd561028c6
Sebastião <sebastiao.alpes@hotmail.com>
2025-06-04 09:30:09 +0100

    upgrade vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Change error to warn for unused variables
+      "@typescript-eslint/no-unused-vars": "warn",
+      
+      // Change error to warn for unescaped entities
+      "react/no-unescaped-entities": "warn",
+      
+      // Change error to warn for any type
+      "@typescript-eslint/no-explicit-any": "warn"
+    }
+  }
 ];
 
 export default eslintConfig;
